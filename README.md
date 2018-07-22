618f477c6dfcd55e10a310f6f18a4034fcd3bb21
memset0 <memset0@outlook.com>
2018-07-22 14:00:59 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # luogu-problem-difficulty
+
 统计（通过）的洛谷题目的难度。
+
+# 安装
+
+使用了一些第三方库，你需要先安装他们。
+
+进入命令行（如果你已经正确安装了`Python3`且配置了路径的话），输入
+
+```bash
+pip install pyyaml
+pip install requests
+```
---
+++ main.py
@@ -0,0 +1,95 @@
+import re
+import yaml
+import requests
+
+# 读取配置文件
+
+config = yaml.load(open('config.yml', 'r+', encoding = 'utf8'))
+# print(config['UserID'])
+
+# 定义全局变量
+
+UserID = config['UserID']
+UserWeb = 'https://www.luogu.org/space/show?uid={}'.format(UserID)
+
+limitCount = config['limitCount']
+
+# 定义全局函数
+
+def writeFile(path, mode, content):
+    file = open(path, mode)
+    file.write(content)
+    file.close()
+
+# 定义 Session
+
+request = requests.Session()
+
+# 列出已经AC的题目
+
+def listAC():
+
+    req  = request.get(UserWeb)
+    html = req.text
+    base = req.content
+
+    writeFile('log/user.html', 'wb+', base)
+
+    html = re.findall(r'<div class="lg-article am-hide-sm">[\s\S]*?</div>', html)[0]
+    html = html.replace('<div class="lg-article am-hide-sm">\n', '')
+    html = html.replace('<h2>通过题目</h2>\n', '')
+    html = html.replace('\n</div>', '')
+    html = re.sub(r'\[<a data-pjax href="/problem/show\?pid=[\s\S]*?">', '', html)
+    html = re.sub(r'</a>\]', '', html)
+    prob = re.split(r'\n', html)
+
+    writeFile('log/AC.txt', 'w+', ", ".join(str(it) for it in prob))
+
+    return prob
+
+# 按题号统计题目难度
+
+def statistics(prob):
+
+    global limitCount
+    ans = []
+
+    for probID in prob:
+
+        print('Statisticsing {}'.format(probID))
+
+        try:
+            probWeb = 'https://www.luogu.org/problemnew/show/{}'.format(probID)
+
+            req  = request.get(probWeb)
+            html = req.text
+            base = req.content
+
+            writeFile('log/prob.html', 'wb+', base)
+
+            dify = re.findall(r'<span class="am-badge am-radius lg-bg-[\S\s]*?">[\s\S]*?</span>', html)[0]
+            dify = re.sub(r'<span class="am-badge am-radius lg-bg-[\S\s]*?">', '', dify)
+            dify = dify.replace('</span>', '')
+
+            ans.append(dify)
+
+            limitCount -= 1
+            if limitCount == 0:
+                break
+
+        except:
+            print('Met a problem at {}'.format(probID))
+
+    return ans
+
+
+if config['hasStatisticsed'] == False:
+    ans = statistics(listAC())
+    out = ", ".join(str(it) for it in ans)
+    writeFile('log/ans.txt', 'w+', out)
+    print(ans)
+else:
+    ans = open('log/ans.txt', 'r+').read().split(', ')
+
+print(config['hasStatisticsed'])
+print('Finish')
