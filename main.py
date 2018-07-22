2314fc3ae4887d5b4eda8728c3e29ab592ff3a0d
memset0 <memset0@outlook.com>
2018-07-22 23:31:37 +0800

    上一个代码没push全。。。

--- main.py
+++ main.py
@@ -2,39 +2,84 @@ import re
 import yaml
 import requests
 
-# 读取配置文件
+# ========== 读取配置文件 & 定义全局变量 ==========
 
-config = yaml.load(open('config.yml', 'r+', encoding = 'utf8'))
-# print(config['UserID'])
+request = requests.Session()
+config  = yaml.load(open('config.yml', 'r+', encoding = 'utf8'))
+debug   = config['debug']
+try:
+    UserID = config['UserID'].split(', ')
+except:
+    UserID = [config['UserID']]
+try:
+    prob = yaml.load(open('result/problem.yml', 'r+', encoding = 'utf8'))
+except:
+    prob = {}
 
-# 定义全局变量
+# ========== 定义全局函数 ==========
 
-UserID = config['UserID']
-UserWeb = 'https://www.luogu.org/space/show?uid={}'.format(UserID)
-
-limitCount = config['limitCount']
-
-# 定义全局函数
+# ---------- 正则表达式截取一段内容 ----------
 
 def strFind(before, after, old):
     return re.findall(r'{}[\s\S]*?{}'.format(before, after), old)[0]\
         .replace(before, '').replace(after, '')
+
+# ---------- 输出内容到文件 ----------
 
 def writeFile(path, mode, content):
     file = open(path, mode)
     file.write(content)
     file.close()
 
-# 定义 Session
+# ---------- 把一些上千数据转换为正常数值 ----------
 
-request = requests.Session()
+def isThousand(old): # 顺便吐槽一下某些大佬的AC数太多了
+    if 'K' in old or 'k' in old:
+        old = old.replace('K', '').replace('k', '')
+        old = int(float(old) * 1000 // 1)
+    return str(old)
 
-# 获取个人信息页面
 
-def getUser():
+# ---------- 获取一道题目的难度 ----------
+
+def getProblem(probID):
+
+    global prob
+
+    if not probID in prob:
+
+        print('Statisticsing {}'.format(probID))
+
+        try:
+            probWeb = 'https://www.luogu.org/problemnew/show/{}'.format(probID)
+
+            req  = request.get(probWeb)
+            html = req.text
+            base = req.content
+
+            if debug:
+                writeFile('log/prob.html', 'wb+', base)
+
+            dify = re.findall(r'<li><strong>难度</strong>[\s\S]*?</span>', html)[0]
+            dify = re.sub(r'<li[\s\S]*">', '', dify)
+            dify = dify.replace('</span>', '')
+
+            prob[probID] = dify 
+            return dify
+
+        except:
+            print('Met a problem at {}'.format(probID))
+            dify = '难度未知'
+
+            prob[probID] = dify 
+            return dify
+
+# ========== 获取个人信息页面 ==========
+
+def getUser(UserID):
 
     if config['hasStatisticsed'] == False:
-        global UserWeb
+        UserWeb = 'https://www.luogu.org/space/show?uid={}'.format(UserID)
         req  = request.get(UserWeb)
         html = req.text
         base = req.content
@@ -47,12 +92,13 @@ def getUser():
         file.close()
         return content
 
-# 列出已经AC的题目
+# ========== 列出已经AC的题目 ==========
 
-def listAC():
+def listAC(UserID):
 
-    html = getUser()
+    print('List AC problems of U{}'.format(UserID))
 
+    html = getUser(UserID)
     html = re.findall(r'<div class="lg-article am-hide-sm">[\s\S]*?</div>', html)[0]
     html = html.replace('<div class="lg-article am-hide-sm">\n', '')
     html = html.replace('<h2>通过题目</h2>\n', '')
@@ -65,48 +111,33 @@ def listAC():
 
     return prob
 
-# 按题号统计题目难度
+# ========== 按题号统计题目难度 ==========
 
 def statistics(prob):
 
-    global limitCount
-    ans = []
+    if config['hasStatisticsed'] == False:
+        ans = []
+        for probID in prob:
+            ans.append(getProblem(probID))
 
-    for probID in prob:
+        out = ", ".join(str(it) for it in ans)
+        writeFile('log/ans.txt', 'w+', out)
 
-        print('Statisticsing {}'.format(probID))
-
-        try:
-            probWeb = 'https://www.luogu.org/problemnew/show/{}'.format(probID)
-
-            req  = request.get(probWeb)
-            html = req.text
-            base = req.content
-
-            writeFile('log/prob.html', 'wb+', base)
-
-            dify = re.findall(r'<li><strong>难度</strong>[\s\S]*?</span>', html)[0]
-            dify = re.sub(r'<li[\s\S]*">', '', dify)
-            dify = dify.replace('</span>', '')
-
-            ans.append(dify)
-
-            limitCount -= 1
-            if limitCount == 0:
-                break
-
-        except:
-            print('Met a problem at {}'.format(probID))
+    else:
+        ans = open('log/ans.txt', 'r+').read().split(', ')
 
     return ans
 
-# 统计输出结果
+# ========== 统计输出结果 ==========
 
 def modify(set, content):
     result = content
     for it in re.findall(r'{\|[\s\S]*?\|}', content):
-        # print('Modify "{}"'.format(it[2:-2]))
-        result = result.replace(it, set[it[2:-2]])
+        try:
+            # print('Modify "{}"'.format(it[2:-2]))
+            result = result.replace(it, set[it[2:-2]])
+        except:
+            pass
     return result
 
 def modifyFile(oldPath, newPath, set):
@@ -118,26 +149,21 @@ def modifyFile(oldPath, newPath, set):
     file.write(content)
     file.close()
 
-def result(ans):
+def result(UserID, ans):
 
-    # 初始化
-    set = {}
+    set  = {}
+    html = getUser(UserID)
 
-    # 从全局变量中引入
-    global UserID, UserWeb
-    set['UserID'] = str(UserID)
-    set['UserWeb'] = UserWeb
+    set['UserID']     = str(UserID)
+    set['UserWeb']    = 'https://www.luogu.org/space/show?uid={}'.format(UserID)
+    set['UserName']   = strFind('<h1>U{} '.format(UserID), '</h1>', html)
+    set['Submit']     = re.sub(r'<[\s\S]*?>', '', strFind('<span class="lg-bignum-num">', '</span><span class="lg-bignum-text">提交</span>', html))
+    set['SubmitReal'] = isThousand(set['Submit'])
+    set['Accept']     = re.sub(r'<[\s\S]*?>', '', strFind('</span></li><li><span class="lg-bignum-num">', '</span><span class="lg-bignum-text">通过</span>', html))
+    set['AcceptReal'] = isThousand(set['Accept'])
+    set['ACpercent']  = str(int(set['AcceptReal']) / int(set['SubmitReal']))
+    set['ACpercent%'] = str(int(set['AcceptReal']) * 100 // int(set['SubmitReal']))
 
-    # 从网页（个人简介页面）中引入
-    html = getUser()
-
-    set['UserName'] = strFind('<h1>U{} '.format(UserID), '</h1>', html)
-    set['Submit'] = strFind('<span class="lg-bignum-num">', '</span><span class="lg-bignum-text">提交</span>', html)
-    set['Accept'] = strFind('</span></li><li><span class="lg-bignum-num">', '</span><span class="lg-bignum-text">通过</span>', html)
-    set['ACpercent'] = str(int(set['Accept']) / int(set['Submit']))
-    set['ACpercent%'] = str(int(set['Accept']) * 100 // int(set['Submit']))
-
-    # 从统计数据中引入
     for it in ans:
         set[it] = 0
     for it in ans:
@@ -145,22 +171,21 @@ def result(ans):
     for it in ans:
         set[it] = str(set[it])
 
-    print(set)
+    print('Finish listing, now this it the information of U{}'.format(UserID), set)
 
-    modifyFile('theme/index.html', 'result.html', set)
-    modifyFile('theme/count.txt', 'result.txt', set)
-    
+    modifyFile('theme/count.txt', 'result/U{}.txt'.format(UserID), set)
 
+# ========== 结束程序 ==========
 
-# 主程序部分
+def finish():
+    global prob
+    writeFile('result/problem.html', 'w+', yaml.dump(prob))
+    print('Finish.')
 
-if config['hasStatisticsed'] == False:
-    ans = statistics(listAC())
-    out = ", ".join(str(it) for it in ans)
-    writeFile('log/ans.txt', 'w+', out)
-else:
-    ans = open('log/ans.txt', 'r+').read().split(', ')
+# ========== 主程序部分 ==========
 
-result(ans)
+for it in UserID:
+    result(it, statistics(listAC(int(it))))
 
-print('Finish')
+finish()
+
